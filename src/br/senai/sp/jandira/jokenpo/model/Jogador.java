d0330180d00c66e174e64874ba7d2a03d57d5836
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2025-10-06 13:26:23 -0300

    Adição de variáveis e da classe Random, e ajuste na classe Scanner

--- src/br/senai/sp/jandira/jokenpo/model/Jogador.java
+++ src/br/senai/sp/jandira/jokenpo/model/Jogador.java
@@ -1,38 +1,44 @@
 package br.senai.sp.jandira.jokenpo.model;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Jogador {
 
-    int numeroDaJogada;
+    int numeroJogada;
+    int numeroJogadaComputador;
     String jogada;
+    String jogadaComputador;
+    String resultado;
+    String decisao;
+    Scanner leitor = new Scanner(System.in);
+    Random random = new Random();
 
     public void receberNumero(){
-        Scanner leitor = new Scanner(System.in);
-
         System.out.println("-------------------------------------------");
-        System.out.println("------ VAMOS JOGAR JOKENPÔ! ------");
+        System.out.println("-------- VAMOS JOGAR JOKENPÔ! --------");
         System.out.println("-------------------------------------------");
-        System.out.println("Escolha uma opção:");
+        System.out.println("Escolha uma opção: ");
         System.out.println("(1) Pedra");
         System.out.println("(2) Papel");
         System.out.println("(3) Tesoura");
         System.out.println("-------------------------------------------");
-        System.out.print("Digite a sua escolha: ");
-        numeroDaJogada = leitor.nextInt();
+        System.out.print("Digite a sua escolha:  ");
+        numeroJogada = leitor.nextInt();
+        leitor.nextLine();
 
         verificarJogada();
     }
 
     public void verificarJogada(){
-        if (numeroDaJogada == 1){
+        if (numeroJogada == 1){
             jogada = "Pedra";
-        } else if (numeroDaJogada == 2){
+        } else if (numeroJogada == 2){
             jogada = "Papel";
-        } else if (numeroDaJogada == 3){
+        } else if (numeroJogada == 3){
             jogada = "Tesoura";
         } else {
-            jogada = "Valor inválido. Jogue novamente.";
+            System.out.println("Valor inválido. Jogue novamente.");
             receberNumero();
         }
     }
